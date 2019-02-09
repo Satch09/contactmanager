1c3ff755be266c1accee03cfb451e0eec9e30e54
Satch09 <christophermaisch@gmail.com>
2019-02-09 09:37:55 +0200

    added contact delete function

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,18 +3,37 @@ import PropTypes from 'prop-types';
 
 class Contact extends Component {
   static propTypes = {
-    contact: PropTypes.object.isRequired
+    contact: PropTypes.object.isRequired,
+    deleteClickHandler: PropTypes.func.isRequired
   };
   state = {
     showContactInfo: false
   };
+
+  onDeleteClick = () => {
+    this.props.deleteClickHandler();
+  }
 
   render() { // You can use this keyword since this it is being used inside of a class which has 'this'
   const {name, phone, email} = this.props.contact;
   const {showContactInfo} = this.state;
     return (
       <div className="card card-body mb-3">
-      <h4>{name} <i onClick={() => this.setState({showContactInfo: !this.state.showContactInfo})} className="fas fa-sort-down"/></h4>
+      <h4>{name}{' '}
+      <i onClick={() => this.setState({showContactInfo: !this.state.showContactInfo})}
+      className="fas fa-sort-down"
+      style={{cursor: 'pointer'}}
+      />
+      <i className="fas fa-times"
+      style={{
+      cursor: 'pointer',
+      float: 'right',
+      color: 'red'
+      }}
+      onClick={this.onDeleteClick}
+      >
+      </i>
+      </h4>
       {showContactInfo ? (
       <ul className="list-group">
         <li className="list-group-item">Email: {email}</li>
