5919dffbb9281d39204b11fe2cdaa0635c7feaa2
Satch09 <christophermaisch@gmail.com>
2019-03-07 20:43:55 +0200

    Updates

---
+++ precache-manifest.ac4d3d4ba3f46ebf3f36ecb5e27e1b7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ba95436e058642002e4",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "6ba95436e058642002e4",
+    "url": "/contactmanager/static/js/main.6ba95436.chunk.js"
+  },
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "15d491e4904cfaeafdc3",
+    "url": "/contactmanager/static/css/2.cc1f2400.chunk.css"
+  },
+  {
+    "revision": "15d491e4904cfaeafdc3",
+    "url": "/contactmanager/static/js/2.15d491e4.chunk.js"
+  },
+  {
+    "revision": "39b75c593b806f1ef89e06045c04c946",
+    "url": "/contactmanager/index.html"
+  }
+];
