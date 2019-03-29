418b3b82f30a2b76da8dfcbda3d26eb09349933b
Satch09 <18578121+Satch09@users.noreply.github.com>
2019-03-29 20:14:30 +0200

    Playing with VIM and finishing off Redux
    I will be going through this section of the course again to fully understand how to implement Redux myself

--- src/components/contacts/EditContact.js
+++ src/components/contacts/EditContact.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import TextInputGroup from '../layout/TextInputGroup';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {getContact} from '../../actions/contactActions';
+import {updateContact} from '../../actions/contactActions';
 
 class EditContact extends Component {
   state = {
@@ -7,6 +11,25 @@ class EditContact extends Component {
     email: '',
     phone: '',
     errors: {}
+  };
+
+  UNSAFE_componentWillReceiveProps(nextProps, nextState) {
+    const {name, email, phone} = nextProps.contact;
+    this.setState({
+      name,
+      email,
+      phone
+    });
+  }
+
+  static propTypes = {
+    contact: PropTypes.object.isRequired,
+    getContact: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    const {id} = this.props.match.params;
+    this.props.getContact(id);
   };
 
   onSubmit = (e) => {
@@ -35,7 +58,6 @@ class EditContact extends Component {
       email,
       phone
     };
-
     const { id } = this.props.match.params;
 
     //// UPDATE CONTACT ////
@@ -98,4 +120,8 @@ class EditContact extends Component {
   }
 }
 
-export default EditContact;
+const mapStateToProps = state => ({
+  contact: state.contact.contact
+});
+
+export default connect(mapStateToProps, {getContact,updateContact})(EditContact);
--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,8 +1,9 @@
-import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from '../actions/types';
+import { GET_CONTACTS, GET_CONTACT, UPDATE_CONTACT, DELETE_CONTACT, ADD_CONTACT } from '../actions/types';
 
 
 const initialState = {
-	contacts: []
+	contacts: [],
+	contact: {}
 };
 
 export default function(state = initialState, action) {
@@ -11,6 +12,11 @@ export default function(state = initialState, action) {
 		return {
 			...state,
 			contacts: action.payload
+		};
+	case GET_CONTACT:
+		return {
+			...state,
+			contact: action.payload
 		};
 	case DELETE_CONTACT:
 		return {
@@ -21,6 +27,11 @@ export default function(state = initialState, action) {
 		return {
 			...state,
 			 contacts: [action.payload, ...state.contacts]
+		};
+	case UPDATE_CONTACT:
+		return {
+			...state,
+			contacts: state.contacts.map(contact => contact.id === action.payload.id ? (contact = action.payload) : contact)
 		};
 	default:
 		return state;
